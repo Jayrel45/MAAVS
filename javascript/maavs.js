d26fef1a6c5b5b9cb97d5a40484e67c14992e6cb
Jayrel45 <51036079+Jayrel45@users.noreply.github.com>
2020-11-15 23:36:56 -0500

    Added Command: prefix

--- javascript/maavs.js
+++ javascript/maavs.js
@@ -2,14 +2,30 @@ const Discord = require('discord.js');
 const token = require('./token');
 const client = new Discord.Client();
 
+var comPrefix = '!';
+
+var prefixList = [
+    '!',
+    '-'
+];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+//Command: Change Prefix
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+var command = msg.content.split(" ");
+  
+  if (command[0] === comPrefix + 'ping') {
     msg.reply('quack');
+  }
+  else if (command[0] === comPrefix + 'prefix') {
+    
+    comPrefix = command[1];
+    msg.reply('Prefix Changed to ' + comPrefix);
+    console.log("prefix changed!");
   }
 });
 
